2b200a1fef6a68b773bc80a7412f8bb7d589e5a1
Dharan0512 <dharanibala0512@gmail.com>
2023-01-05 23:09:23 +0530

    filter changed

--- client/src/components/FilterModal.js
+++ client/src/components/FilterModal.js
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Modal from 'react-bootstrap/Modal';
+import Slider from '@mui/material/Slider'
 import {useState} from 'react';
 import { Button } from '@mui/material';
+import styled from 'styled-components';
 
 function FilterModal(props, onFilter) {
   const [isActive, setIsActive] = useState(false);
   const [isVeganActive, setVeganActive] = useState(false);
+  const [selectedValue, setSelectedValue] = useState([0]);
 
   const handleChange = (event)=>{
+
     switch (event.target.name) {
       case 'isActive':
         console.log('veg');
@@ -25,8 +29,22 @@ function FilterModal(props, onFilter) {
   const handleSubmit = (event)=>{
     event.preventDefault();
     onFilter({isActive, isVeganActive})
+  }
+
+  const handleChangeValue = (event, value)=>{
+    setSelectedValue(value);
+  }
+
+  const applyFilters = ()=>{
+    //Price Filters
+    const minValue = selectedValue[0];
+    const maxValue = selectedValue[1];
     
   }
+
+  // useEffect(()=>{
+  //   applyFilters();
+  // },[selectedValue])
 
   return (
     <Modal {...props} size="md" centered>
@@ -61,6 +79,20 @@ function FilterModal(props, onFilter) {
              &nbsp; Vegan
             </label>
           </div>
+          <div>
+            <SlidersCss>
+            <label htmlFor='is-actives' className='health-title'>
+             &nbsp; Healthiness
+            </label>
+              <Slider value={selectedValue}
+              onChange={handleChangeValue}
+              valueLabelDisplay='on'
+              min={0}
+              // max={100}
+              size="medium"
+              />
+            </SlidersCss>
+          </div>
         </form>
       </Modal.Body>
       <Modal.Footer>
@@ -69,5 +101,15 @@ function FilterModal(props, onFilter) {
     </Modal>
   )
 }
+
+
+const SlidersCss = styled.div`
+  display: block;
+  margin-right: 20rem;
+
+  .health-title{
+    margin-top: .5rem
+  }
+`
 
 export default FilterModal
